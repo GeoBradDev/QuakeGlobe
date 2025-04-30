66964a8390b946cb39329514b214f263a00500bc
GeoBradDev <Brad.Stricherz@gmail.com>
2025-04-30 13:38:26 -0400

    Globe and API call is working.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    define: {
+        CESIUM_BASE_URL: JSON.stringify('/cesium'),
+    },
+    resolve: {
+        alias: {
+            cesium: resolve(__dirname, 'node_modules/cesium'),
+        },
+    },
 })
