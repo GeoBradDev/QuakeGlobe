90f35d3169c0abda5081a0139b9a33e180920c24
GeoBradDev <Brad.Stricherz@gmail.com>
2025-05-01 11:27:41 -0400

    timeviewer is starting to work

--- src/components/CeiumGlobe.tsx
+++ src/components/CeiumGlobe.tsx
@@ -1,8 +1,20 @@
 import {Viewer, Entity} from "resium"
-import {Cartesian3, Color, createWorldTerrainAsync, TerrainProvider, Ion} from "cesium"
-import {useEffect, useState} from "react"
+import {
+    Cartesian3,
+    Color,
+    createWorldTerrainAsync,
+    TerrainProvider,
+    Ion,
+    Clock,
+    JulianDate,
+    ClockRange,
+    ClockViewModel
+} from "cesium"
+import {useEffect, useRef, useState} from "react"
 import FilterPanel from "./FilterPanel.tsx"
 import MagnitudeLegend from "./MagnitudeLegend.tsx";
+import {Viewer as CesiumViewer} from "cesium"
+
 
 Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_TOKEN
 
@@ -52,7 +64,10 @@ export default function CesiumGlobe() {
     const [terrainProvider, setTerrainProvider] = useState<TerrainProvider | undefined>()
     const [magRange, setMagRange] = useState<[number, number]>([0, 10])
     const [depthRange, setDepthRange] = useState<[number, number]>([0, 700])
+    const [clock, setClock] = useState<Clock>()
+    const [currentTime, setCurrentTime] = useState<JulianDate | undefined>()
 
+    const viewerRef = useRef<CesiumViewer | null>(null)
 
     useEffect(() => {
         createWorldTerrainAsync().then(setTerrainProvider)
@@ -74,6 +89,39 @@ export default function CesiumGlobe() {
           })
     }, [])
 
+    useEffect(() => {
+        if (quakes.length > 0) {
+            const sorted = [...quakes].sort((a, b) => a.time - b.time)
+            const start = JulianDate.fromDate(new Date(sorted[0].time))
+            const stop = JulianDate.fromDate(new Date(sorted[sorted.length - 1].time))
+            console.log("Clock start:", start.toString())
+            console.log("Clock stop:", stop.toString())
+
+
+            if (JulianDate.lessThan(start, stop)) {
+                const newClock = new Clock({
+                    startTime: start,
+                    currentTime: JulianDate.clone(start),
+                    stopTime: stop,
+                    clockRange: ClockRange.LOOP_STOP,
+                    multiplier: 10000,
+                    shouldAnimate: false,
+                })
+
+                const handleClockTick = (c: Clock) => {
+                    setCurrentTime(JulianDate.clone(c.currentTime))
+                }
+
+                newClock.onTick.addEventListener(handleClockTick)
+
+                setClock(newClock)
+
+            } else {
+                console.warn("Invalid time range: start and stop are equal")
+            }
+        }
+    }, [quakes])
+    
     return (
       <>
           <FilterPanel
@@ -86,10 +134,27 @@ export default function CesiumGlobe() {
           />
 
           <MagnitudeLegend/>
-          <Viewer full terrainProvider={terrainProvider}>
-              {quakes
-                .filter((q) => q.magnitude >= magRange[0] && q.magnitude <= magRange[1] &&
-                  q.coordinates[2] <= depthRange[1] && q.coordinates[2] <= depthRange[1])
+          <Viewer full terrainProvider={terrainProvider} clockViewModel={clock ? new ClockViewModel(clock) : undefined}
+                  timeline={true} animation={true} ref={viewerRef} onReady={(viewer) => {
+              if (clock) {
+                  viewer.timeline.zoomTo(clock.startTime, clock.stopTime)
+              }
+          }}>
+              {quakes.filter((q) => {
+                  if (!currentTime) return true
+
+                  const quakeTime = JulianDate.fromDate(new Date(q.time))
+
+                  return (
+                    q.magnitude >= magRange[0] &&
+                    q.magnitude <= magRange[1] &&
+                    q.coordinates[2] >= depthRange[0] &&
+                    q.coordinates[2] <= depthRange[1] &&
+                    JulianDate.lessThanOrEquals(quakeTime, currentTime)
+                  )
+              })
+
+
                 .map((q) => (
                   <Entity
                     key={q.id}
